29c935a4e8a71c768bca106c8b255b20711a1109
PotentialKillScreen <axelahlqvist1995@gmail.com>
2022-11-06 18:02:51 +0100

    output is put into csv file

--- compDraft.cpp
+++ compDraft.cpp
@@ -1,27 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void FactionsIntoGroups(vector<string> factions, int nGroups){
+void FactionsIntoGroups(vector<string> factions, int nGroups, ofstream& myfile){
     int nFactions = factions.size();
     int smallGroup = nFactions/nGroups;
     int nBigGroups = nFactions % nGroups;
+    //ofstream myfile;
+    //myfile.open("output.csv");
     int i = 1; 
     for (;i<nBigGroups + 1; i++){
         cout << "Group " << i << ": ";
         for (int j = 0; j< smallGroup + 1; j++){
             cout << factions.back(); 
+            myfile << factions.back() << ",";
             factions.pop_back();
             if (j < smallGroup) {
                cout  << ", ";  
             }
             
+            
         }
         cout << endl;
+        myfile << "\n";
     }
     while(!factions.empty()){
         cout << "Group " << i << ": ";
         for (int j = 0; j < smallGroup; j++){
             cout << factions.back();
+            myfile << factions.back() << ",";
             factions.pop_back();
             if (j < smallGroup-1) {
                cout  << ", ";  
@@ -29,6 +35,7 @@ void FactionsIntoGroups(vector<string> factions, int nGroups){
         }
         i++;
         cout << endl;
+        myfile << "\n";
     }
 }
 
@@ -39,15 +46,22 @@ int main(){
     random_device rd;
     mt19937 g(rd());
     shuffle(names.begin(),names.end(),g);
+    remove("output.csv");
+    ofstream myfile;
+    myfile.open("output.csv");
     for(int i = 0; i<names.size();i++){
         cout << "Val " << i+1 << ": " << names[i] << endl;
+        myfile << names[i] << "\n";
     }
     for(int i = names.size()-1; i>=0;i--){
         cout << "Val " << names.size()*2 -i << ": " << names[i] << endl;
+        myfile << names[i] << "\n";
     }
     for(int i = names.size()-1; i>=0;i--){
         cout << "Val " << names.size()*3-i << ": " << names[i] << endl;
+        myfile << names[i] << "\n";
     }
+    myfile << "\n";
     cout << endl;
     vector<string> factions{};
     vector<string> vanillaFactions ={"Arborec", "Barony of Letnev", "Clan of Saar", "Embers of Muaat", "Emirates of Hacan", "Federation of Sol", "Ghost of Creuss", "L1Z1X Mindnet" ,"Mentak Coalition", "Naalu Collective", "Nekro Virus", "Sardakk N'orr", "Universities of Jol-Nar", "Winnu", "Xxcha Kingdom", "Yin Brotherhood", "Yssaril Tribes"};
@@ -63,7 +77,7 @@ int main(){
     mt19937 g2(rd2());
     shuffle(factions.begin(),factions.end(),g2);
     
-    FactionsIntoGroups(factions, names.size()); 
+    FactionsIntoGroups(factions, names.size(), myfile); 
     return 0;
 }
 
